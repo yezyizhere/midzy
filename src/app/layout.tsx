b21ae8a01a666aacd8de6dd72dfd68de8a84a22a
MIDZY <itmidnal0212@gmail.com>
2026-04-19 08:21:45 +0000

    change root

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,14 +2,31 @@ import "./globals.css";
 import { type Metadata } from "next";
 
 export const metadata: Metadata = {
-  metadataBase: new URL("http://localhost:3000"),
+  metadataBase: new URL(
+    process.env.NEXT_PUBLIC_BASE_URL || 
+    (process.env.VERCEL_PROJECT_PRODUCTION_URL ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}` : 
+     process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : 
+     "http://localhost:3000")
+  ),
   title: "MIDZY",
   description: "ITZY Fan Platform",
   openGraph: {
     title: "MIDZY",
     description: "ITZY Fan Platform",
-    images: ["/picture/thumbnail.jpg"],
+    images: [
+      {
+        url: "/picture/thumbnail.jpg",
+        width: 1200,
+        height: 630,
+      },
+    ],
     type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "MIDZY",
+    description: "ITZY Fan Platform",
+    images: ["/picture/thumbnail.jpg"],
   },
   icons: {
     icon: "/picture/logo.png",
